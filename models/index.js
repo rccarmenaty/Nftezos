afda651cfac9e3d3272239d2cf6b43e2cfc80092
Ramon <rccarmenaty@icloud.com>
2022-12-27 22:17:14 +0100

    Fixing travis and Dockerfile

--- models/index.js
+++ models/index.js
@@ -10,17 +10,17 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
+
+console.log("Configuration: ", config);
+
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config, {
-    logging: false,
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
-    config,
-    { logging: false }
+    config
   );
 }
 
